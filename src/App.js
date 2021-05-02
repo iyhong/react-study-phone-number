c99a4e4d628c6687e5c9b99986f1a873a8252a27
loverman85 <loverman85@hanmail.net>
2021-05-02 16:32:59 +0900

    feat: 수정 기능구현  - handleToggleEdit  - handleChange  - onUpdate

--- src/App.js
+++ src/App.js
@@ -9,14 +9,31 @@ function App() {
 
   let age = 5;
   // let id = useState(0)
-  const id = useState(0)
+  const id = useState(3)
   let getId = id[0];
   const setId = id[1];
   
 
-  const information = useState([]);
+  const information = useState([
+    {
+      id: 0,
+      name: '홍길동',
+      phone: '010-2233-1455'
+    },
+    {
+      id: 1,
+      name: '김미림',
+      phone: '010-1111-0035'
+    },
+    {
+      id: 2,
+      name: '김민준',
+      phone: '010-2113-1000'
+    }
+  ]);
   const arrInfo = information[0];
   const setInfo = information[1];
+
 
   const handleCreate = (data) => {
     console.log(getId)
@@ -33,12 +50,20 @@ function App() {
     setInfo(arrInfo.filter(info => info.id !== id))
   }
 
+  const handleUpdate = (id, data) =>{
+    setInfo(arrInfo.map(info => {
+      if(info.id === id) return {id, ...data}
+      else return info;
+    }))
+  }
+
   return (
     <div className="App">
       <PhoneForm onCreate={handleCreate}/>
       <PhoneInfoList 
         data={arrInfo}
         onRemove={handleRemove}
+        onUpdate={handleUpdate}
         />
     </div>
   );
--- src/components/PhoneInfo.js
+++ src/components/PhoneInfo.js
@@ -1,15 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 export default class PhoneInfo extends Component {
+
+  state = {
+    editing: false,
+    name: '',
+    phone: '',
+  }
 
   handleRemove = () => {
     const {info, onRemove} = this.props;
     onRemove(info.id);
   }
-  
+
+  handleToggleEdit = () => {
+    const {info, onUpdate } = this.props;
+    // 수정하고 적용버튼 누를 때
+    if(this.state.editing){
+      onUpdate(info.id, {
+        name: this.state.name,
+        phone: this.state.phone,
+      })
+    }else{
+      this.setState({
+        name: info.name,
+        phone: info.phone
+      })
+    }
+    this.setState({
+      editing: !this.state.editing
+    });
+
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
     
     const { name, phone, id } = this.props.info;
+    const { editing } = this.state; 
 
     const style = {
       border: '1px solid black',
@@ -17,17 +50,27 @@ export default class PhoneInfo extends Component {
       margin: '8px', 
     }
 
+    console.log(name)
+
     return (
       <div style={style}>
-        <div>
-          <b>{name}</b>
-        </div>    
-        <div>
-          {phone}
-        </div>
-        <div>
-          <button onClick={this.handleRemove}>삭제</button>
-        </div>
+        {
+          editing ? 
+          (
+          <Fragment>
+            <div><input name="name" onChange={this.handleChange} value={this.state.name}/></div>
+            <div><input name="phone" onChange={this.handleChange} value={this.state.phone}/></div>
+          </Fragment>
+          ) : (
+          <Fragment>
+            <div><b>{name}</b></div>    
+            <div>{phone}</div>
+          </Fragment>
+          )
+        }
+        <button onClick={this.handleRemove}>삭제</button>
+        <button onClick={this.handleToggleEdit}>{editing? '적용': '수정'}</button>
+        
       </div>
     )
   }
